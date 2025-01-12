c368836b6b99cac6711915e47f129a68d028a317
Simone <150595274+simonetimi@users.noreply.github.com>
2025-01-12 10:19:37 +0100

    refactor code and fix bug where black brush wouldn't paint on rainbow ink

--- script.js
+++ script.js
@@ -1,202 +1,82 @@
-const grid = document.querySelector('.grid');
-let gridSize = '16';
+const grid = document.querySelector(".grid");
+let gridSize = 16;
+let currentEffect = null;
 
 function getRandomHexColor() {
-    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    while (randomColor.length < 6) {
-      randomColor = '0' + randomColor;
-    }
-    return '#' + randomColor;
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  while (randomColor.length < 6) {
+    randomColor = "0" + randomColor;
   }
-
-// 16x16 grid
-function addGrid16 () {   
-    for (let i = 0; i < 256; i++) {
-        const gridBlock = document.createElement('div');
-        grid.appendChild(gridBlock);
-        gridBlock.classList.add('grid-block-16');
-        gridBlock.classList.add('white-block');
-        gridBlock.addEventListener('mouseover', function () {
-            if (rainbowEffect === false && shadowEffect === false) {
-                gridBlock.classList.add('black-block');
-                gridBlock.classList.remove('white-block');
-            } else if (rainbowEffect === true) {
-                gridBlock.classList.remove('white-block');
-                gridBlock.style.backgroundColor = getRandomHexColor();
-            } else if (shadowEffect === true) {
-                gridBlock.classList.remove('white-block');
-                gridBlock.classList.add('black-block');
-                let opacity = parseFloat(gridBlock.style.opacity) || 0.0;
-                opacity += 0.1;
-                if (opacity > 1) {
-                    opacity = 1;
-                }           
-                gridBlock.style.opacity = opacity.toString();
-            }
-        });
-    }
-    gridSize = '16';
+  return "#" + randomColor;
 }
-window.addEventListener('DOMContentLoaded', addGrid16);
+
+function addGrid() {
+  const totalBlocks = Math.pow(gridSize, 2);
+  for (let i = 0; i < totalBlocks; i++) {
+    const gridBlock = document.createElement("div");
+    grid.appendChild(gridBlock);
+    gridBlock.classList.add("grid-block-" + gridSize);
+    gridBlock.classList.add("white-block");
+    gridBlock.addEventListener("mouseover", function () {
+      if (currentEffect === "rainbow") {
+        gridBlock.classList.remove("white-block");
+        gridBlock.style.backgroundColor = getRandomHexColor();
+      } else if (currentEffect === "shadow") {
+        gridBlock.style.backgroundColor = "";
+        gridBlock.classList.remove("white-block");
+        gridBlock.classList.add("black-block");
+        let opacity = parseFloat(gridBlock.style.opacity) || 0.0;
+        opacity += 0.1;
+        if (opacity > 1) {
+          opacity = 1;
+        }
+        gridBlock.style.opacity = opacity.toString();
+      } else {
+        gridBlock.style.backgroundColor = "";
+        gridBlock.classList.remove("white-block");
+        gridBlock.classList.add("black-block");
+      }
+    });
+  }
+}
+window.addEventListener("DOMContentLoaded", addGrid);
 
 function resetGrid() {
-    while (grid.firstChild) {
-        grid.removeChild(grid.firstChild);
-    }
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
 }
 
-const button16 = document.querySelector('#b16');
-button16.addEventListener('click', () => {
+// set buttons for different sizes
+for (let i = 16; i <= 128; i *= 2) {
+  const button = document.querySelector("#b" + i);
+  button.addEventListener("click", () => {
     resetGrid();
-    addGrid16();
-});
-
-// 32x32 grid
-
-function addGrid32 () {   
-    for (let i = 0; i < 1024; i++) {
-        const gridBlock = document.createElement('div');
-        grid.appendChild(gridBlock);
-        gridBlock.classList.add('grid-block-32');
-        gridBlock.classList.add('white-block');
-        gridBlock.addEventListener('mouseover', function () {
-            if (rainbowEffect === false && shadowEffect === false) {
-                gridBlock.classList.add('black-block');
-                gridBlock.classList.remove('white-block');
-            } else if (rainbowEffect === true) {
-                gridBlock.classList.remove('white-block');
-                gridBlock.style.backgroundColor = getRandomHexColor();
-            } else if (shadowEffect === true) {
-                gridBlock.classList.remove('white-block');
-                gridBlock.classList.add('black-block');
-                let opacity = parseFloat(gridBlock.style.opacity) || 0.0;
-                opacity += 0.1;
-                if (opacity > 1) {
-                    opacity = 1;
-                }           
-                gridBlock.style.opacity = opacity.toString();
-            }
-        });
-    }
-    gridSize = '32';
+    gridSize = i;
+    addGrid();
+  });
 }
-
-const button32 = document.querySelector('#b32');
-button32.addEventListener('click', () => {
-    resetGrid();
-    addGrid32();
-});
-
-// 64x64 grid
-
-function addGrid64 () {   
-    for (let i = 0; i < 4096; i++) {
-        const gridBlock = document.createElement('div');
-        grid.appendChild(gridBlock);
-        gridBlock.classList.add('grid-block-64');
-        gridBlock.classList.add('white-block');
-        gridBlock.addEventListener('mouseover', function () {
-            if (rainbowEffect === false && shadowEffect === false) {
-                gridBlock.classList.add('black-block');
-                gridBlock.classList.remove('white-block');
-            } else if (rainbowEffect === true) {
-                gridBlock.classList.remove('white-block');
-                gridBlock.style.backgroundColor = getRandomHexColor();
-            } else if (shadowEffect === true) {
-                gridBlock.classList.remove('white-block');
-                gridBlock.classList.add('black-block');
-                let opacity = parseFloat(gridBlock.style.opacity) || 0.0;
-                opacity += 0.1;
-                if (opacity > 1) {
-                    opacity = 1;
-                }           
-                gridBlock.style.opacity = opacity.toString();
-            }
-        });
-    }
-    gridSize = '64';
-}
-
-const button64 = document.querySelector('#b64');
-button64.addEventListener('click', () => {
-    resetGrid();
-    addGrid64();
-});
-
-// 128x128 grid
-
-function addGrid128 () {   
-    for (let i = 0; i < 16384; i++) {
-        const gridBlock = document.createElement('div');
-        grid.appendChild(gridBlock);
-        gridBlock.classList.add('grid-block-128');
-        gridBlock.classList.add('white-block');
-        gridBlock.addEventListener('mouseover', function () {
-            if (rainbowEffect === false && shadowEffect === false) {
-                gridBlock.classList.add('black-block');
-                gridBlock.classList.remove('white-block');
-            } else if (rainbowEffect === true) {
-                gridBlock.classList.remove('white-block');
-                gridBlock.style.backgroundColor = getRandomHexColor();
-            } else if (shadowEffect === true) {
-                gridBlock.classList.remove('white-block');
-                gridBlock.classList.add('black-block');
-                let opacity = parseFloat(gridBlock.style.opacity) || 0.0;
-                opacity += 0.1;
-                if (opacity > 1) {
-                    opacity = 1;
-                }           
-                gridBlock.style.opacity = opacity.toString();
-            }
-        });
-    }
-    gridSize = '128';
-}
-
-const button128 = document.querySelector('#b128');
-button128.addEventListener('click', () => {
-    resetGrid();
-    addGrid128();
-});
-
-let rainbowEffect = false;
-let shadowEffect = false;
 
 // black button
-const buttonReset = document.querySelector('#black');
-buttonReset.addEventListener('click', () => {
-    rainbowEffect = false;
-    shadowEffect = false;
+const buttonReset = document.querySelector("#black");
+buttonReset.addEventListener("click", () => {
+  currentEffect = null;
 });
 
 // rainbow effect button
-
-const buttonRainbow = document.querySelector('#rainbow');
-buttonRainbow.addEventListener('click', () => {
-    rainbowEffect = true;
-    shadowEffect = false;
+const buttonRainbow = document.querySelector("#rainbow");
+buttonRainbow.addEventListener("click", () => {
+  currentEffect = "rainbow";
 });
 
 //shadow effect button
-const buttonShadow = document.querySelector('#shadow');
-buttonShadow.addEventListener('click', () => {
-    rainbowEffect = false;
-    shadowEffect = true;
+const buttonShadow = document.querySelector("#shadow");
+buttonShadow.addEventListener("click", () => {
+  currentEffect = "shadow";
 });
 
-const buttonClear = document.querySelector('#clear');
-buttonClear.addEventListener('click', () => {
-    if (gridSize === '16') {
-        resetGrid();
-        addGrid16();
-    } else if (gridSize === '32') {
-        resetGrid();
-        addGrid32();
-    } else if (gridSize === '64') {
-        resetGrid();
-        addGrid64();
-    } else if (gridSize === '128') {
-        resetGrid();
-        addGrid128();
-    }
+const buttonClear = document.querySelector("#clear");
+buttonClear.addEventListener("click", () => {
+  resetGrid();
+  addGrid();
 });
